2180e041766f6be7ac95e1ff043febe59008c653
SavinovaIren <irka887@gmail.com>
2022-10-12 23:18:45 +0300

    +UserListView и UserCreateView не работает

--- ads/views.py
+++ ads/views.py
@@ -8,7 +8,7 @@ from django.views.generic import DetailView, CreateView, ListView, UpdateView, D
 import json
 
 from HW27 import settings
-from ads.models import Category, Ads, User
+from ads.models import Category, Ads, User, Location
 
 
 def main(request):
@@ -218,3 +218,55 @@ class AdsDeleteView(DeleteView):
         super().delete(request, *args, **kwargs)
 
         return JsonResponse({"status": "ok"}, status=200)
+
+
+class UserListView(ListView):
+    model = User
+    queryset = User.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        self.object_list.order_by("username")
+        paginator = Paginator(object_list=self.object_list, per_page=settings.TOTAL_ON_PAGE)
+        page = request.GET.get("page")
+        page_obj = paginator.get_page(page)
+
+        response = []
+        for user in page_obj:
+            response.append({
+                'id': user.id,
+                'username': user.username,
+                'password': user.password,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'role': user.role,
+                'age': user.age,
+            })
+        return JsonResponse({'ads': response, 'pages': page_obj.number, 'total': page_obj.paginator.count}, safe=False,
+                            json_dumps_params={'ensure_ascii': False})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UserCreateView(CreateView):
+    model = User
+    fields = ['username', 'password', 'first_name', 'last_name', 'role', 'location_id']
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        user = User.objects.create(username=data['username'],
+                                   password=data['password'],
+                                   first_name=data['first_name'],
+                                   last_name=data['last_name'],
+                                   role=data['role'])
+        for loc in data['location_id']:
+            location = Location.objects.get_or_create(name=loc)
+            user.location.add(location)
+        return JsonResponse({'id': user.id,
+                             'username': user.username,
+                             'password': user.password,
+                             'first_name': user.first_name,
+                             'last_name': user.last_name,
+                             'role': user.role,
+                             'locations': [str(u) for u in user.location.all()]},
+                            safe=False, json_dumps_params={'ensure_ascii': False})
